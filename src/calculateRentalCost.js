2c22ebc1b32c581e0e7620ef21ecd7dbf401bf90
BohdanTrue <rapsbodik@gmail.com>
2024-03-11 18:39:39 +0200

    first version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,13 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+
+  if (days >= 3 && days < 7) {
+    return days * PRICE_PER_DAY - 20;
+  } else if (days >= 7) {
+    return days * PRICE_PER_DAY - 50;
+  } else {
+    return days * PRICE_PER_DAY;
+  }
 }
 
 module.exports = calculateRentalCost;
